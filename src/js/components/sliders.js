a553c6d56df51b4e0aed376e16032e0351b87833
hoverr-dev <scadygamesru@gmail.com>
2022-10-02 16:46:53 +0300

    added: adaptive for website

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -19,6 +19,29 @@ export function slider() {
       enabled: true,
       onlyInViewport: false,
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5
+      },
+      480: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 2
+      },
+      992: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      1024: {
+        slidesPerView: 2.29,
+        slidesPerGroup: 2
+      }
+    },
     modules: [Navigation, Pagination, Keyboard],
   });
 }
